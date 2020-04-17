673ceab11ef5f72fe1a42d1959dad2e3a6a09b97
Vicente <vnvega@uc.cl>
2020-04-17 18:23:00 -0400

    Navegacion con permisos para user local y match

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :local_users
   devise_for :match_users
   root 'static_pages#home'
-  get 'static_pages/home'
+  get '/login' ,to: 'static_pages#login'
+  get '/match_home' ,to: 'static_pages#match_home'
+  get '/local_home' ,to: 'static_pages#local_home'
 end
